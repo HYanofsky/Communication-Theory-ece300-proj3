8697b0aa47938b1329f52070476a5c362512145c
Jonathan Lam <jlam55555@gmail.com>
2020-12-14 00:20:45 -0500

    changed standard_array to output syndromes rather than standard array

--- standard_array.m
+++ standard_array.m
@@ -1,40 +1,48 @@
+%% This file generates the syndrome arrays S1 and S2 (corresponding to
+% codes c1, c2)
+clc; clear; close all;
+
 % sac = standard array correctable
+load('code.mat');
 
 %% code 1
 corr1 = 1;
 k1 = 4;
 n1 = 8;
-sac_1 = zeros(8, 2^k1, n1);
+sac_1 = zeros(8+1, 2^k1, n1);
+
+% first row of standard array is no errors
+sac_1(1, :, :) = c1;
 
 I = eye(n1);
 
 for i = 1:n1
-    sac_1(i, :, :) = galois2_add(c1, I(i, :));
+    sac_1(i+1, :, :) = galois2_add(c1, I(i, :));
 end
 
-trunc_sac_1 = sac_1(:, 1, :);
-
-fprintf('Size of correctable standard array 1: %d\n', numel(sac_1)/n1);
-fprintf('Size of truncated correctable standard array 1: %d\n', ...
-    numel(trunc_sac_1)/n1);
+% create 
+errors1 = reshape(sac_1(:, 1, :), [], n1);
+S1 = galois2_multiply(errors1, parity_check(G1).');
 
 %% code 2
 corr2 = 2;
 k2 = 4;
 n2 = 12;
-sac_2 = zeros(12+12*11/2, 2^k2, n2);
+sac_2 = zeros(1+12+12*11/2, 2^k2, n2);
 
 % 12
 I = eye(n2);
 
-counter = 1;
+% first row of standard array is no errors
+sac_2(1, :, :) = c2;
+
+counter = 2;
 
 for i = 1:n2
     sac_2(counter, :, :) = galois2_add(c2, I(i, :));
     counter = counter + 1;
 end
 
-% something like 12x11 codewords = 132
 for i = 2:n2
     mat = I(i, :);
     for j = 1:i-1
@@ -43,8 +51,8 @@ for i = 2:n2
     end
 end
 
-trunc_sac_2 = sac_2(:, 1, :);
+errors2 = reshape(sac_2(:, 1, :), [], n2);
+S2 = galois2_multiply(errors2, parity_check(G2).');
 
-fprintf('Size of correctable standard array 2: %d\n', numel(sac_2)/n2);
-fprintf('Size of truncated correctable standard array 2: %d\n', ...
-    numel(trunc_sac_2)/n2);
+%% save to .mat file
+save('syndrome.mat', 'S1', 'S2');
