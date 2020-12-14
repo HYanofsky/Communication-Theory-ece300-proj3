001c5d423b11b8cc044f5b7473211414adf179c7
Jonathan Lam <jlam55555@gmail.com>
2020-12-14 00:20:54 -0500

    cleaned up parity_check.m

--- parity_check.m
+++ parity_check.m
@@ -1,6 +1,6 @@
-function H=parity_check(G)
-    [k,n]=size(G);
-    P=G(:, [k+1: end]);
-    I=eye(n-k);
-    H=[transpose(P) I];
+function H = parity_check(G)
+    [k,n] = size(G);
+    P = G(:, (k+1):end);
+    I = eye(n - k);
+    H = [P.' I];
 end
