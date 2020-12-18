47cdccbfe091afdead4d6421c4755821c19e707c
Jonathan Lam <jlam55555@gmail.com>
2020-12-18 18:06:23 -0500

    shortened lines on correct_errors.m so that they fit better

--- correct_errors.m
+++ correct_errors.m
@@ -1,4 +1,6 @@
-% correct_errors: returns corrected codeword if correctable, otherwise NaN
+% correct_errors: returns corrected codeword if correctable,
+% otherwise NaN
+%
 % params:
 % SE_map= container.Map instance mapping syndromes to errors
 % H     = parity check matrix for the code
@@ -6,14 +8,15 @@
 % returns:
 % Xhat  = estimated codeword if correctable, else NaN
 function Xhat = correct_errors(SE_map, H, X)
-    % hashmap can only be accessed linearly (not vectorizable), so this
-    % has to be implemented in a loop
+    % hashmap can only be accessed linearly (not vectorizable),
+    % so this has to be implemented in a loop
     Xhat = zeros(size(X));
     for i = 1:size(X, 1)
         x = X(i, :);
         try
             Xhat(i,:) = galois2_add(x, ...
-                de2bi(SE_map(bi2de(galois2_multiply(x, H.'))), length(x)));
+                de2bi(SE_map(bi2de(...
+                    galois2_multiply(x, H.'))), length(x)));
         catch
             % if not found in SE_map
             Xhat(i,:) = NaN * ones(size(x));
